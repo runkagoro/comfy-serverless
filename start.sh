82ce6e9e11cee84efe387dde05cbb4440a738c50
runkagoro <udenolih819@gmail.com>
2026-04-19 20:48:56 +0300

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "🚀 Запуск ComfyUI в фоновом режиме..."
-python main.py --listen 127.0.0.1 --port 8188 &
-
-echo "⏳ Даем ComfyUI 10 секунд на загрузку нод..."
-sleep 10
-
-echo "🤖 Запуск кастомного обработчика API..."
-python custom_handler.py
